58c8536df49363ccae0fc183ca415d21108013d7
Yamaan Nandolia <yamaannandolia@gmail.com>
2024-01-08 22:30:57 -0600

    Added some comments on js file

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+/**
+* flip: When the user clicks the appropriate buttons on the card it flips to show/hide the info
+*/
 flip = () => {
     document.getElementById('card').classList.toggle('flipped')
     document.querySelector('#front .reflection').classList.toggle('move')
     document.querySelector('#back .reflection').classList.toggle('move')
-}
+} //end of flip()
 
-document.getElementById('show-btn').addEventListener('click', flip)
-document.getElementById('hide-btn').addEventListener('click', flip)
+document.getElementById('show-btn').addEventListener('click', flip) //click listener on the appropriate button
+document.getElementById('hide-btn').addEventListener('click', flip) //click listener on the appropriate button
